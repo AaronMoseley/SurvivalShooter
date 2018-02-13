a212fbb4acc6965ac5a7a588267a66eecb39c31c
AaronMoseley <moseleyaj11@yahoo.com>
2018-02-13 18:41:43 -0500

    init commit

---
+++ Assets/Scripts/Player/Medkit.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Medkit : MonoBehaviour {
+
+	PlayerHealth playerHealth;
+	GameObject player;
+	bool playerTouching;
+	public int HealAmount = 20;
+
+
+	void Awake ()
+	{
+		Medkit = GameObject.FindGameObjectWithTag ("medkit");
+		playerHealth = player.GetComponent <PlayerHealth> ();
+	}
+	
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject == player)
+		{
+			playerTouching = true;
+		}
+	}
+
+	void Update () {
+		if (playerTouching && playerHealth.currentHealth < 120) 
+		{
+			Heal ();
+		}
+	}
+
+	void Heal ()
+	{
+		if (playerHealth.currentHealth < 120) 
+		{
+			playerHealth.Heal (HealAmount);
+		}
+	}
+}
